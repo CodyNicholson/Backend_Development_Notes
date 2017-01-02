d9dd726fca0e2348176114b710e5980118409aea
Cody Nicholson <codynicholson96@gmail.com>
2017-01-02 16:04:38 -0600

    Finished MVC fundamentals exercise2

--- The_Complete_ASP.NET_MVC_5_Course/Final_Project/Vidly/Controllers/MoviesController.cs
+++ The_Complete_ASP.NET_MVC_5_Course/Final_Project/Vidly/Controllers/MoviesController.cs
@@ -51,15 +51,20 @@ namespace Vidly.Controllers
             return Content("id="+id);
         }
 
-        // int? means that this int parameter it nullable
-        public ActionResult Index(int? pageIndex, string sortBy)
+        public ViewResult Index()
         {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            var movies = GetMovies();
 
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
         }
 
         [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
