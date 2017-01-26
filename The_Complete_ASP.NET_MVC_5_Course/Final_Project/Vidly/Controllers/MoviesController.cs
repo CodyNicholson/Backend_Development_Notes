ea4be1fd9db2e4f2bfd09410630474a5b899cfbb
Cody Nicholson <codynicholson96@gmail.com>
2017-01-26 16:34:51 -0600

    Added validation

--- The_Complete_ASP.NET_MVC_5_Course/Final_Project/Vidly/Controllers/MoviesController.cs
+++ The_Complete_ASP.NET_MVC_5_Course/Final_Project/Vidly/Controllers/MoviesController.cs
@@ -61,8 +61,20 @@ namespace Vidly.Controllers
         //    return View(movie);
         //}
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new MovieFormViewModel(movie)
+                {
+                    Genres = _context.Genres.ToList()
+                };
+
+                return View("MovieForm", viewModel);
+            }
+
             if (movie.Id == 0)
             {
                 movie.DateAdded = DateTime.Now;
@@ -71,7 +83,6 @@ namespace Vidly.Controllers
             else
             {
                 var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
-
                 movieInDb.Name = movie.Name;
                 movieInDb.ReleaseDate = movie.ReleaseDate;
                 movieInDb.GenreId = movie.GenreId;
@@ -104,9 +115,8 @@ namespace Vidly.Controllers
                 return HttpNotFound();
             }
 
-            var viewModel = new MovieFormViewModel()
+            var viewModel = new MovieFormViewModel(movie)
             {
-                Movie = movie,
                 Genres = _context.Genres.ToList()
             };
 
